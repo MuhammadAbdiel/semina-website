3b2839f7b2d24a1691992de09b36a051acba4069
MuhammadAbdiel <abdielfirdie@gmail.com>
2024-07-05 16:44:02 +0700

    use cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 // middlewares
 const notFoundMiddleware = require('./app/middleware/not-found');
@@ -23,6 +24,7 @@ const app = express();
 
 const v1 = '/api/v1';
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
